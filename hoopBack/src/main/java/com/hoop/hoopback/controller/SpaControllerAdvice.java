96ec55594c21863cfb13850b02c1e52f677adff0
Shah Imenov <dapizda1029@gmail.com>
2026-04-14 10:35:28 +0600

    update spa advice

--- hoopBack/src/main/java/com/hoop/hoopback/controller/SpaControllerAdvice.java
+++ hoopBack/src/main/java/com/hoop/hoopback/controller/SpaControllerAdvice.java
@@ -1,11 +1,14 @@
 package com.hoop.hoopback.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class SpaControllerAdvice {
 
     @ExceptionHandler(NoResourceFoundException.class)
